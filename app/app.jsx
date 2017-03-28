78f0b06c3df1862b2881269d3098634728c4a037
Avinash Singh <avinash4507@gmail.com>
2017-03-28 07:24:35 +0530

    Redux store created

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,17 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var TodoApp = require('TodoApp');
+var actions = require('actions');
+var store = require('store').configure();
+
+store.subscribe(() => {
+  console.log(store.getState())
+});
+
+store.dispatch(actions.searchTodo('name'))
+store.dispatch(actions.toggleShowCompleted())
+store.dispatch(actions.addTodo('running'))
+
 // Load foundation
 $(document).foundation();
 
